4477affc05b3fd414e759f10168db7008fa9a6be
cyan <iamian3102@gmail.com>
2021-09-28 16:57:28 +0800

    add production express.static()

--- backend/index.js
+++ backend/index.js
@@ -26,5 +26,14 @@ app.use(passport.session());
 app.use('/auth', authRoutes);
 app.use('/api/products', productRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+
+  const path = require('path');
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
